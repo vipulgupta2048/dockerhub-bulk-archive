a854095f802947a5002e4d36d7b083a135253684
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2025-05-09 01:13:43 +0530

    No timeout for tests
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './automation',
-  timeout: 30000,
+  timeout: 900000000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
